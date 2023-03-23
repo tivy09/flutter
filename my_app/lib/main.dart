ac10de7e5fd8130037e725513b0039e4d0b641b8
tivy09 <teeivy63@gmail.com>
2023-03-23 10:47:01 +0800

    UPDATE

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -15,30 +15,21 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const QuizPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<QuizPage> createState() => _QuizPage();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _QuizPage extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,14 +49,17 @@ class DiceWidget extends StatelessWidget {
   const DiceWidget({super.key});
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-              child: TextButton(
-                  onPressed: () {}, child: Image.asset('lib/assets/1.jpg')))
-        ],
-      ),
-    );
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children:[
+        Expanded(
+          flex:7,
+          child: Center(child: Text('This is were the question is going to go'
+          style: TextStyle(color: Colors.white70,fontSize: 20),
+          textAlign: TextAlign.center,),
+
+        ))
+      ],
+    )
   }
 }
