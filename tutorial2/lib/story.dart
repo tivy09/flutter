63f10e2a74d2509df554f330718412b3d4a63248
tivy09 <teeivy63@gmail.com>
2023-04-06 12:59:12 +0800

    UPDATE BMI and ADD tutorial2

---
+++ tutorial2/lib/story.dart
@@ -0,0 +1,7 @@
+// we want to pair up the question and answer instead of having two separate lists: 1 list for questions and 1 list for answers
+class Story {
+  String story;
+  bool answer;
+
+  Story({required this.story, required this.answer});
+}
